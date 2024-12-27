70582104cd4523549c08a95f9e90a671372ae379
RaccoonLasagna <102906286+RaccoonLasagna@users.noreply.github.com>
2024-12-27 17:09:07 +0700

    q1 folder created

--- q1/simpledrawingwindow1.py
+++ q1/simpledrawingwindow1.py
@@ -24,7 +24,7 @@ class Simple_drawing_window1(QWidget):
         p.end()
 def main():
     app=QApplication(sys.argv)
-    w=Simple_drawing_window()
+    w=Simple_drawing_window1()
     w.show()
     return app.exec()
 if __name__=="__main__":
